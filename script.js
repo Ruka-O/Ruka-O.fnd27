ac4387f9afc116cc3bef0db3ed24fbacd9a8d403
Ruka-O <166612479+Ruka-O@users.noreply.github.com>
2024-05-26 22:22:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,67 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-// function test(actual, expected) {
-//     if (JSON.stringify(actual) === JSON.stringify(expected)) {
-//         console.log("OK! Test PASSED.");
-//     } else {
-//         console.error("Test FAILED. Try again!");
-//         console.log("    actual: ", actual);
-//         console.log("  expected: ", expected);
-//         console.trace();
-//     }
-// }
-
 // 正解かどうか
 function random() {
-  const dialog = document.querySelector = ("dialog");
-  const resultComment = document.getElementById("old-result");
-  const resultRecord = document.getElementById("result-record");
+  const selectNumber = document.getElementById("select-number").value;
   const betOCT = document.getElementById("bet-oct").value;
-  const myOCT = document.getElementById("oct");
   const establish = Math.floor(Math.random() * 100);
   let rate = 0;
-  console.log(betOCT);
-  for (const famous in data) {
-    rate += data[famous];
+  for (const object of animalData) {
+    rate += object.weight;
     if (establish <= rate) {
-      result = famous;
+      result = object.Number;
       break;
     }
   }
-  const selectNumber = document.getElementById("select-number").value;
-  if (result === selectNumber) {
+  dialog.showModal();
+  gameconut += 1;
+  if (result === Number(selectNumber)) {
     comment = "🎉おめでとうございます🎉";
     victorycount += 1;
-    // out -= betOCT * odds;  //oddsはhtml上に表示しているものを持ってくる
+    differenceOct = Number(betOCT) * (animalData[result - 1].Odds.innerText - 1);
+    oct += differenceOct;
+    myOCT.innerText = `💰 ${oct.toLocaleString()} OCT (+${differenceOct.toLocaleString()})`;
+    recordComment = `${gameconut}回目 結果：${result}  予想:${selectNumber}  ${comment}`;
+    changeOdds = `${differenceOct.toLocaleString()} OCTプラス`;
+    face.src = "happy.gif";
   } else {
     comment = "残念😭";
     losecount += 1;
-    oct -= Number(betOCT);
-
+    differenceOct = Number(betOCT);
+    oct -= differenceOct;
+    myOCT.innerText = `💰 ${oct.toLocaleString()} OCT (▲${differenceOct.toLocaleString()})`;
+    recordComment = `${gameconut}回目 結果：${result}  予想:${selectNumber}  ${comment}`;
+    changeOdds = `${differenceOct.toLocaleString()} OCTマイナス`;
+    face.src = "cry.gif";
   }
-  gameconut += 1;
-  // dialog.show();
-  resultComment.innerText = `  《これまでの結果》        ${victorycount}勝${losecount}敗`;
-  const creatPalagraph = document.createElement("p");
-  creatPalagraph.innerText = `${gameconut}回目 結果：${result}  予想:${selectNumber}  ${comment}`;
-  resultRecord.prepend(creatPalagraph);
-  myOCT.innerText = `💰 ${oct.toLocaleString()} OCT`;
 }
 
+function oneResult() {
+  dialog.close();
+  const oneResultComment = document.getElementById("oneresult-comment");
+  oneResultComment.innerText = `${comment} \n 優勝は${animalData[result - 1].Name}でした。\n ${changeOdds}されました。`;
+  dialogClose.showModal();
+  resultComment.innerText = `  《これまでの結果》        ${victorycount}勝${losecount}敗`;
+  const creatPalagraph = document.createElement("p");
+  creatPalagraph.innerText = recordComment;
+  resultRecord.prepend(creatPalagraph);
+  calculate();
+  if (oct <= 0) {
+    dialogClose.close();
+    bankHelp.showModal();
+    nowOCT.innerText = `${oct} OCT`;
+  } 
+}
+
+function borrowOCTBank() {
+  const borrowOCT = document.getElementById("borrow-oct").value;
+  oct += Number(borrowOCT);
+  debtSum += Number(borrowOCT);
+  myOCT.innerText = `💰 ${oct.toLocaleString()} OCT (▲${differenceOct.toLocaleString()})`;
+  debt.innerText = `借金:${debtSum.toLocaleString()} OCT`;
+  bankBorrow.close();
+}
 
 function randomOfOdds(minNum, maxNum) {
   let odds = 0;
@@ -56,13 +69,53 @@ function randomOfOdds(minNum, maxNum) {
   return parseFloat(odds.toFixed(1));
 }
 
-// オッズの計算
-const odds = [
-  {min: 7.1, max: 10},
-  {min: 5.1, max: 7},
-  {min: 2.6, max: 4},
-  {min: 1.6, max: 2.5},
-  {min: 1, max: 1.5}
+const penguinOdds = document.getElementById("penguin-odds");
+const raccoonsOdds = document.getElementById("raccoons-odds");
+const pandaOdds = document.getElementById("panda-odds");
+const pigOdds = document.getElementById("pig-odds");
+const dogOdds = document.getElementById("dog-odds");
+
+const animalData = [
+  {
+    Name: "ペンギン",
+    Number: 1,
+    Odds: penguinOdds,
+    minOdds: 7.1,
+    maxOdds: 10,
+    weight: 5
+  },
+  {
+    Name: "たぬき",
+    Number: 2,
+    Odds: raccoonsOdds,
+    minOdds: 7.1,
+    maxOdds: 10,
+    weight: 5
+  },
+  {
+    Name: "パンダ",
+    Number: 3,
+    Odds: pandaOdds,
+    minOdds: 2.6,
+    maxOdds: 5,
+    weight: 15
+  },
+  {
+    Name: "ぶた",
+    Number: 4,
+    Odds: pigOdds,
+    minOdds: 1.6,
+    maxOdds: 2.5,
+    weight: 25
+  },
+  {
+    Name: "いぬ",
+    Number: 5,
+    Odds: dogOdds,
+    minOdds: 1,
+    maxOdds: 1.5,
+    weight: 45
+  }
 ];
 
 // 重みづけの配列
@@ -86,24 +139,68 @@ const passwordCandidates = [
   "akayubotau"
 ];
 
+function calculate() {
+  for (let i = 0; i < animalData.length; i++) {
+    let calculatedOdds = 0;
+    calculatedOdds = randomOfOdds(animalData[i].minOdds, animalData[i].maxOdds);
+    animalData[i].Odds.innerText = calculatedOdds;
+  }
+}
 
 
 let gameconut = 0;
 let victorycount = 0;
 let losecount = 0;
+let differenceOct = 0;
+let borrowCount = 0;
+let debtSum = 0;
+let changeOdds = "";
 let comment = "";
 let result = "";
+let recordComment = "";
 let oct = 1000;
 let index = Math.floor(Math.random() * 8);    //パスワード設定
+
+calculate();
+
+const myOCT = document.getElementById("oct");
 const password = passwordCandidates[index];
-
 const startButton = document.getElementById("start-button");
-
-
+const dialog = document.getElementById("dialog-open");
 const recordOpen = document.getElementById("result-open");
+const resultComment = document.getElementById("old-result");
+const resultRecord = document.getElementById("result-record");
 startButton.addEventListener("click", random);
-// startButton.addEventListener("click", resultOpen, false);
 if (index === gameconut) {
   // パスワードを告知
   // クリック箇所も提示
 }
+const resultOpen = document.getElementById("result-open");
+const resultClose = document.getElementById("result-close");
+const dialogClose = document.getElementById("dialog-close");
+const bankHelp = document.getElementById("bank-help");
+const bankOpen = document.getElementById("bank-open");
+const bankBorrow = document.getElementById("bank-borrow");
+const borrow = document.getElementById("borrow");
+const gamefinish = document.getElementById("finish");
+const nowOCT = document.getElementById("now-oct");
+const debt = document.getElementById("debt");
+const face = document.getElementById("face");
+const passwordShow = document.getElementById("password-show");
+const passwordOpen = document.getElementById("password");
+const passwordShowClose = document.getElementById("password-show-close");
+resultOpen.addEventListener("click",oneResult);
+resultClose.addEventListener("click", function () {
+  dialogClose.close();
+  if (gameconut === 3){
+    passwordShow.showModal();
+    passwordOpen.innerText = password;
+  }
+});
+bankOpen.addEventListener("click",function(){
+  bankHelp.close();
+  bankBorrow.showModal()});
+borrow.addEventListener("click", borrowOCTBank);
+gamefinish.addEventListener("click", function(){location.reload()});
+passwordShowClose.addEventListener("click", function(){passwordShow.close()});
+
